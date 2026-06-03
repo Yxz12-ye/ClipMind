ac5d04d96515d027f191753518b09aefce5f0f99
Yxz12-ye <3078961710@qq.com>
2026-06-03 13:06:33 +0800

    feat: 完成Windows端剪切板的监听

--- src/service/CopyEventListener.cpp
+++ src/service/CopyEventListener.cpp
@@ -2,20 +2,75 @@
 
 #ifdef WIN32
 
-#endif
-
 bool WindowsCopyEventListener::registerListenService() 
 {
     if (AddClipboardFormatListener(hwnd)){
         // 此处需要接入Debug信息
+        qDebug()<<"成功添加剪贴板监听";
     }
     else{
         // 此处需要接入Debug信息
+        qDebug()<<"无法添加剪贴板监听";
+        return false;
     }
+    return true;
 }
 
-WindowsCopyEventListener::WindowsCopyEventListener(QWidget* topWidget, QObject* parent)
+bool WindowsCopyEventListener::handleNativeEvent(void* message, qintptr* result) {
+    MSG *msg = reinterpret_cast<MSG*>(message);
+    if (msg->message == WM_CLIPBOARDUPDATE) {
+        // 剪贴板已更新，发出信号
+        emit clipboardChanged();
+        if (result) *result = 0;
+        return true; // 消息已处理
+    }
+    Q_UNUSED(result);
+    return false;
+}
+
+WindowsCopyEventListener::WindowsCopyEventListener(QObject* parent)
     : AbstractCopyEventListener(parent) {
-    hwnd = reinterpret_cast<HWND>(topWidget->winId());
+    m_hiddenWindow = new HiddenWindow(nullptr);
+    m_hiddenWindow->setAttribute(Qt::WA_NativeWindow);
+    m_hiddenWindow->setGeometry(0, 0, 1, 1);
+    m_hiddenWindow->setVisible(false);
+    m_hiddenWindow->m_listener = this;
+
+    hwnd = reinterpret_cast<HWND>(m_hiddenWindow->winId());
+    connect(this, &AbstractCopyEventListener::clipboardChanged, this, &WindowsCopyEventListener::getClipboardText);
     registerListenService();
+}
+
+WindowsCopyEventListener::~WindowsCopyEventListener() {
+    if (m_hiddenWindow) m_hiddenWindow->deleteLater();
+    if (hwnd) RemoveClipboardFormatListener(hwnd);
+}
+
+void WindowsCopyEventListener::getClipboardText() {
+    if (!OpenClipboard(hwnd)) return;
+    if (IsClipboardFormatAvailable(CF_UNICODETEXT)) {
+        HANDLE hData = GetClipboardData(CF_UNICODETEXT); // 获取句柄
+        if (hData) {
+            wchar_t* pwchData = (wchar_t*)GlobalLock(hData); // 锁定内存
+            if (pwchData) {
+                lastText = QString::fromWCharArray(pwchData); // 复制数据
+                GlobalUnlock(hData); // 解锁内存
+            }
+        }
+    }
+    CloseClipboard();
+    return;
+}
+
+HiddenWindow::HiddenWindow(QWidget* parent) :QWidget(parent) {}
+
+bool HiddenWindow::nativeEvent(const QByteArray& eventType, void* message, qintptr* result) {
+    if (m_listener && m_listener->handleNativeEvent(message, result)) return true;
+    return QWidget::nativeEvent(eventType, message, result);
+}
+
+#endif
+
+QString AbstractCopyEventListener::text() {
+    return lastText;
 }
--- src/service/CopyEventListener.hpp
+++ src/service/CopyEventListener.hpp
@@ -8,11 +8,19 @@ class AbstractCopyEventListener : public QObject
     Q_OBJECT
 private:
     virtual bool registerListenService();
+    
+
 public:
     AbstractCopyEventListener(QObject* parent = nullptr);
     virtual ~AbstractCopyEventListener() = default;
+
+    QString lastText;
+    QString text();
+    
 signals:
-    void onCopyEventTrigered();
+    void clipboardChanged();
+public slots:
+    virtual void getClipboardText();
 };
 
 #ifdef WIN32
@@ -23,12 +31,31 @@ signals:
  */
 class WindowsCopyEventListener : public AbstractCopyEventListener
 {
+    Q_OBJECT
 private:
+    HiddenWindow* m_hiddenWindow;
     HWND hwnd;
     bool registerListenService() override;
+private slots:
+    void getClipboardText() override;
+
 public:
-    WindowsCopyEventListener(QWidget* topWidget, QObject* parent = nullptr);
-    ~WindowsCopyEventListener() = default;
+    WindowsCopyEventListener(QObject* parent = nullptr);
+    ~WindowsCopyEventListener();
+
+    bool handleNativeEvent(void *message, qintptr *result);
+    
+};
+
+class HiddenWindow : public QWidget
+{
+private:
+public:
+    WindowsCopyEventListener* m_listener;
+    HiddenWindow(QWidget* parent = nullptr);
+    ~HiddenWindow() = default;
+
+    bool nativeEvent(const QByteArray &eventType, void *message, qintptr *result) override;
 };
 
 #endif
